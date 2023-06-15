1a094d68f37351ac8f991e1f402223481155fdec
DHEFL <dhefl334@gmail.com>
2023-06-15 02:32:34 +0200

    update3

--- non-IID_MNIST/partitionMNIST.sh
+++ non-IID_MNIST/partitionMNIST.sh
@@ -1,7 +1,7 @@
-python3 partitionMNIST.py --n_parties=80 \
+python3 partitionMNIST.py --n_parties=10 \
   --partition='noniid-#label4' \
   --beta=0.5\
   --datadir="MNIST/train.csv" \
-  --outputdir="MNIST/80clients/" \
+  --outputdir="MNIST/10clients/" \
   --init_seed=0
-read
+$SHELL
